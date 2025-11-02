2ca570cfdedbfb565b15252931733ac8bcdfae2d
Basanta Swain <basantswainn@gmail.com>
2025-11-02 13:36:27 +0530

    first commit

---
+++ src/main/java/com/bizbridge/backend/BizBridgeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bizbridge.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizBridgeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizBridgeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bizbridge/backend/BizBridgeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bizbridge.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizBridgeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
